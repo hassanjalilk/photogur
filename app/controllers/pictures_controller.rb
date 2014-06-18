0372bf9fea7f1dcdc434eff5b170c1572f01a9c1
hassanjalilk <hjalil.hba2014@ivey.ca>
2014-06-18 15:06:37 -0400

    making pictures show at /pictures

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,4 +1,25 @@
-class Picturescontroller < ApplicationController
+class PicturesController < ApplicationController
 	def index
+
+		@pictures = [
+
+			{
+				:title => "The old church on the coast of the White sea" ,
+				:artist => " Sergey Ershov" ,
+				:url => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
+				}, 
+
+				{
+					:title => " Sea Power",
+					:artist => "Stephen Scullion",
+					:url => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"
+					},
+
+					{
+						:title => "Into the poppies",
+						:artist => "John Wilhelm",
+						:url => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"
+						}
+					]
 	end
 end
